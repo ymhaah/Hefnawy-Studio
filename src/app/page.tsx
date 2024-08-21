accc7e642a6a76b0313d490234aba916f74980e4
Youssef Hafnawy <77534098+ymhaah@users.noreply.github.com>
2024-08-21 03:40:40 +0300

    build: adding the following package (gsap -sonner - framer-motion - shadcn ui+it's dependencies )

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+
+export default function Home() {
+    return <main className="">hefnawy studio</main>;
+}
+
+// TODO: the global css
+// TODO: take what you need form the code starter (style, components, etc)
