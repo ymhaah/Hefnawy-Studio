fe2e6fa9d106a7f5da430965532dba3d03e9f15b
Youssef Hefnawy <ymhaah250@gmail.com>
2025-01-16 11:31:27 +0200

    style: working on the loading & logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 // import Image from "next/image";
 import Loader from "../components/Loader";
+import { Button } from "@ui/Button";
 
 export default function Home() {
     return (
         <main className="h-[500vh] w-full">
             <Loader />
-            <div className="Container"></div>
+            <div className="Container">
+                <Button>test</Button>
+            </div>
         </main>
     );
 }
--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,4 +1,4 @@
-"use client"; // Make sure to add this for Next.js 13+ with App Router
+// "use client";
 
 function Loader() {
     return (
@@ -7,96 +7,14 @@ function Loader() {
             aria-live="assertive"
         >
             <div className="Container flex items-center justify-center">
-                <span className="h">Hefnawy</span> |{" "}
-                <span className="s">Studio</span>
+                {/* <h1 className="animated-element">حَفْناوِيٍّ</h1> */}
+                <h1>
+                    <span className="h">حَ</span>
+                    sْtُuْdِiُo
+                </h1>
             </div>
         </div>
     );
 }
 
-// function Loader() {
-//     return (
-//         <div
-//             className=" bg-light-bg"
-//             aria-live="assertive"
-//         >
-//             <div className="relative w-[15vw] h-[15vw] filter blur-sm contrast-[10]">
-//                 <div className="absolute inset-0">
-//                     <div className="relative w-full h-full small-dots">
-//                         {Array.from({ length: 9 }).map((_, i) => (
-//                             <span
-//                                 key={i}
-//                                 className="absolute w-[12%] h-[12%] bg-dark rounded-full"
-//                                 style={{
-//                                     transform: `translate(
-//                       calc(50% + calc(50% * cos(${(360 / 9) * i}deg))),
-//                       calc(50% + calc(50% * sin(${(360 / 9) * i}deg)))
-//                     )`,
-//                                 }}
-//                             ></span>
-//                         ))}
-//                     </div>
-//                 </div>
-//                 <div className="absolute inset-0">
-//                     <div className="relative w-full h-full big-dots">
-//                         {Array.from({ length: 3 }).map((_, i) => (
-//                             <span
-//                                 key={i}
-//                                 className="absolute w-[18%] h-[18%] bg-dark rounded-full"
-//                                 style={{
-//                                     transform: `translate(
-//                       calc(50% + calc(50% * cos(${(360 / 3) * i}deg))),
-//                       calc(50% + calc(50% * sin(${(360 / 3) * i}deg)))
-//                     )`,
-//                                 }}
-//                             ></span>
-//                         ))}
-//                     </div>
-//                 </div>
-//             </div>
-//         </div>
-//     );
-// }
-
 export default Loader;
-
-// function Loader() {
-//     return (
-//         <div className="loader" aria-live="assertive">
-//             <div className="loading-spinner">
-//                 <div className="absolute">
-//                     <div className="small-dots circle spinner">
-//                         <span></span>
-//                         <span></span>
-//                         <span></span>
-//                         <span></span>
-//                         <span></span>
-//                         <span></span>
-//                         <span></span>
-//                         <span></span>
-//                         <span></span>
-//                     </div>
-//                 </div>
-//                 <div className="absolute">
-//                     <div className="big-dots circle spinner">
-//                         <span></span>
-//                         <span></span>
-//                         <span></span>
-//                     </div>
-//                 </div>
-//             </div>
-
-//             {/* <div className="Container">
-//                 <p>0%</p>
-//                 <div className="loading-par" aria-label="loading par">
-//                     <div className="skeleton">
-//                         <span role="progressbar"></span>
-//                     </div>
-//                 </div>
-//                 <p>100%</p>
-//             </div> */}
-//         </div>
-//     );
-// }
-
-// export default Loader;
