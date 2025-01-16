a96af40ec5a1cd32b8b273bdfd76c5f1acc84175
Youssef Hefnawy <ymhaah250@gmail.com>
2025-01-16 21:19:42 +0200

    style: glitch effect #1

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,20 +1,25 @@
-// "use client";
-
 function Loader() {
+    let logo1 = "حَفْناوِيٍّ";
+    let logo2 = "Studio";
     return (
         <div
             className="Loader fixed w-full h-svh inset-0  z-[1000]"
             aria-live="assertive"
         >
             <div className="Container flex items-center justify-center">
-                {/* <h1 className="animated-element">حَفْناوِيٍّ</h1> */}
-                <h1>
-                    <span className="h">حَ</span>
-                    sْtُuْdِiُo
-                </h1>
+                <div className="loader_logo--1">
+                    <h1 className="glitch" data-text={logo1}>
+                        {logo1}
+                    </h1>
+                </div>
+                {/* <div className="loader_logo--2" data-text={logo2}>
+                    {logo2}
+                </div> */}
             </div>
         </div>
     );
 }
+
+// sْtُuْdِiُo | حَ
 
 export default Loader;
