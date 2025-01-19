39cef3be737b2acdec69fd845b55a506193e9662
Youssef Hefnawy <ymhaah250@gmail.com>
2025-01-19 17:21:38 +0200

    style: some work on the glitch effect

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,19 +1,22 @@
 function Loader() {
     let logo1 = "حَفْناوِيٍّ";
-    let logo2 = "Studio";
+    // let logo1 = "حَ";
+    let logo2 = "studio";
     return (
         <div
             className="Loader fixed w-full h-svh inset-0  z-[1000]"
             aria-live="assertive"
         >
             <div className="Container flex items-center justify-center">
-                <div className="loader_logo--1">
+                <div className="Logo logo_stage-1">
                     <h1 className="glitch" data-text={logo1}>
                         {logo1}
                     </h1>
                 </div>
-                {/* <div className="loader_logo--2" data-text={logo2}>
-                    {logo2}
+                {/* <div className="Logo logo_stage-2">
+                    <h1 className="glitch" data-text={logo2}>
+                        {logo2}
+                    </h1>
                 </div> */}
             </div>
         </div>
